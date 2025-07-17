03147770e3a31f578140e6857a75393adddd3485
APerkinsVT <aperkinsvt@gmail.com>
2025-07-17 17:42:39 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ export const metadata: Metadata = {
   title: 'YouCantFireMe.co',
   description: 'Generate hilarious AI resignation letters instantly.',
   generator: 'YouCantFireMe',
-  icon: "/favicon.ico"
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
