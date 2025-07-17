8f39dfca9eb065604bc694679d2fdcfee00dc475
APerkinsVT <aperkinsvt@gmail.com>
2025-07-17 17:25:27 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'YouCantFireMe.co,
+  title: 'YouCantFireMe.co',
   description: 'Generate hilarious AI resignation letters instantly.',
   generator: 'YouCantFireMe',
   icon: "/favicon.ico"
