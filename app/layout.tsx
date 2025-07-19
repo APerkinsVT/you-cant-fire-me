b290cad7bf93125a9bcbbc916bc8f01ff1a0cfb8
APerkinsVT <aperkinsvt@gmail.com>
2025-07-19 08:30:59 -0400

    Update layout.tsx added Google Analytics via analytics.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Analytics from './analytics'
 
 export const metadata: Metadata = {
   title: 'YouCantFireMe.co',
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Analytics />
+        {children}
+      </body>
     </html>
   )
 }
